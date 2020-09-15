9d3837c1e3512104004be691d15eb0e6f7463be5
Travis Lee <travisslee223@gmail.com>
2020-09-15 14:15:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |season, holidays|
+  
+    end
+  end  
 end
 
 def all_holidays_with_bbq(holiday_hash)
